fa90c24499f80aaf62ed9205feb3ab64a94522df
2303289 <2303289@students.wits.ac.za>
2022-03-26 21:44:16 +0200

    Created a main python file as a basis to work with

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+#Initialising PyGame
+pygame.init()
+
+#Creating the pygame screen
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("2D Minecraft")
+
+#Game runing variable
+game_running = True
+
+#main game loop:
+while game_running:
+    for events in pygame.event.get():    
+        if events.type == pygame.QUIT:
+            game_running = False
