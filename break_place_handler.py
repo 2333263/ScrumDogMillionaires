b0d7947a488b9376916c1e5f81400d0e825119f7
LazarusGroves <101668253+LazarusGroves@users.noreply.github.com>
2022-03-28 20:01:15 +0200

    Optimisation and cleanups + placing draft

--- break_place_handler.py
+++ break_place_handler.py
@@ -4,15 +4,23 @@ import inventory_handler
 import math
 from block import Block
 
-def getPos(pos): #Takes in pygame position coordinates returns block coordinates
-    pos = math.floor(pos[0]/gs.block_size),math.floor(pos[1]/gs.block_size)
+def getPos(pos): #Takes in pygame position coordinates returns block coordinates based system ---> returns block_size*floor(pyPos/block_size) tuple transform
+    pos = gs.block_size*math.floor(pos[0]/gs.block_size),gs.block_size*math.floor(pos[1]/gs.block_size)
     return pos
 
-def block_break(python_pos,world_block):
+def block_break(python_pos,world_block): #Block breaking logic, and inventory handler passover
     pos = getPos(python_pos)
-    temp_block = Block(gs.block_size, (pos[0]*gs.block_size, pos[1]*gs.block_size), gs.customColours["sky"])
     for block in world_block:
-        if block.blockPosition == temp_block.blockPosition:
+        if block.blockPosition == pos:
             world_block.remove(block)
-            block = temp_block
     #inventory TODO
+
+def block_place(python_pos,world_block): #Block placing logic, and inventory handler requesting
+    pos = getPos(python_pos)
+    found = False
+    for block in world_block:
+        if block.blockPosition == pos:
+            found = True
+    if found == False:
+        temp_block = Block(gs.block_size, pos, gs.customColours["grass"])
+        world_block.append(temp_block)   
