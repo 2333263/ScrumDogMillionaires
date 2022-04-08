4e042d2d83820299e8b46aa4849e9ca96a446ccb
2333263 <101044489+2333263@users.noreply.github.com>
2022-04-08 15:24:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import unittest
 
 class TestMethods(unittest.TestCase):
-   def __init__(self):
-      self.test_add()
    def test_add(self):
       self.assertEqual("test1","test1")
 
       
-Test=TestMethods()
+if __name_=='__main__':
+   unittest.main()
