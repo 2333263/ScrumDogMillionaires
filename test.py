f39f990ba24655429a3486931c83e2ac9adb7f2f
2303289 <2303289@students.wits.ac.za>
2022-04-28 16:50:00 +0200

    Added item test cases

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
 import unittest
 import item 
+import gameSettings
 
-# class TestMethods(unittest.TestCase):
-#    def test_add(self):
-#       tempOneItem = item.Item("Grass", 0)
-#       tempTwoItem = item.Item("Grass", 0)
-#       self.assertEqual(tempOneItem.itemName, tempTwoItem.itemName)
 
 
 class TestItem(unittest.TestCase):
    tempItem = item.Item("Grass", 0)
    def test_itemID(self):
       self.assertTrue(type(self.tempItem.itemID) is int)
-      
+      self.assertGreaterEqual(self.tempItem.itemID, 0)
+      self.assertLessEqual(self.tempItem.itemID, len(gameSettings.itemIDs) + 1)
+
    def test_amount(self):
       self.assertTrue(type(self.tempItem.amount) is int)
       self.assertGreaterEqual(self.tempItem.amount, 0)
+
+      oldCount = self.tempItem.amount
+      self.tempItem.increase()
+      self.assertGreaterEqual(self.tempItem.amount, oldCount)
+      self.tempItem.decrease()
+      self.assertEqual(self.tempItem.amount, oldCount)
+      self.assertEqual(self.tempItem.amount, self.tempItem.getCount())
+
+   def test_name(self):
+      self.assertTrue(type(self.tempItem.itemName) is str)
+
+   #Need to check the textures load a valid item  
 
 
 unittest.main()
