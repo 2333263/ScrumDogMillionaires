352e0b66335714698ce7c75e83483cc4c1769f34
2303289 <2303289@students.wits.ac.za>
2022-04-28 17:12:47 +0200

    Added tests for Block class

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import unittest
 import item 
-import gameSettings
-
-
+import gameSettings as gs
+import block
+import pygame
 
 class TestItem(unittest.TestCase):
    tempItem = item.Item("Grass", 0)
    def test_itemID(self):
-      self.assertTrue(type(self.tempItem.itemID) is int)
+      self.assertIsInstance(self.tempItem.itemID, int)
       self.assertGreaterEqual(self.tempItem.itemID, 0)
-      self.assertLessEqual(self.tempItem.itemID, len(gameSettings.itemIDs) + 1)
+      self.assertLessEqual(self.tempItem.itemID, len(gs.itemIDs) + 1)
 
    def test_amount(self):
-      self.assertTrue(type(self.tempItem.amount) is int)
+      self.assertIsInstance(self.tempItem.amount, int)
       self.assertGreaterEqual(self.tempItem.amount, 0)
 
       oldCount = self.tempItem.amount
@@ -23,9 +23,26 @@ class TestItem(unittest.TestCase):
       self.assertEqual(self.tempItem.amount, self.tempItem.getCount())
 
    def test_name(self):
-      self.assertTrue(type(self.tempItem.itemName) is str)
+      self.assertIsInstance(self.tempItem.itemName, str)
 
-   #Need to check the textures load a valid item  
 
+
+class TestBlock(unittest.TestCase):
+   tempBlock = block.Block(gs.blockSize, (0, 0), 0, gs.textureNames[gs.itemIDs[0]])
+   def test_itemIDs(self):
+      self.assertIsInstance(self.tempBlock.itemID, int)
+      self.assertGreaterEqual(self.tempBlock.itemID, 0)
+      self.assertLessEqual(self.tempBlock.itemID, len(gs.itemIDs) + 1)
+
+   def test_positions(self):
+      self.assertGreaterEqual(self.tempBlock.blockPosition[0], 0)
+      self.assertGreaterEqual(self.tempBlock.blockPosition[1], 0)
+
+      self.assertLessEqual(self.tempBlock.blockPosition[0], gs.width)
+      self.assertLessEqual(self.tempBlock.blockPosition[1], gs.height)
+
+   def test_texture(self):
+      self.assertIsInstance(self.tempBlock.textureName,  str)
+      #Add check for texture object
 
 unittest.main()
