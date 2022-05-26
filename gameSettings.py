a0b4f9c3f0f3c93c5e1a3cd8dba19503421a40f2
LazarusGroves <101668253+LazarusGroves@users.noreply.github.com>
2022-05-26 17:39:03 +0200

    update

--- gameSettings.py
+++ gameSettings.py
@@ -4,9 +4,6 @@ import random
 seed = random.randint(-10000, 10000)
 octaves = 1
 
-with open('Levels/' + levelName + '.txt') as f:
-    levelArray = f.readlines()
-
 blockSize = 20 #tested values: [12, 16, 20,24, 32]otherwise collision issues (possibly all multiples of 4 work)
 playerRange = 7
 
