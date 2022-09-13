f19f19b5999414bf54e6b51d0ddafdfdd7b5b82d
2333263 <2333263@students.wits.ac.za>
2022-09-13 13:53:07 +0200

    fixed not running on pcs

--- gym_MC/envs/custom_env.py
+++ gym_MC/envs/custom_env.py
@@ -13,7 +13,7 @@ class CustomEnv(gym.Env):
     def reset(self):
         del self.pygame
         self.pygame = MinePy()
-        obs = self.pygame.observe()
+        obs = np.array(self.pygame.observe(),dtype=np.float32)
         return obs
 
     def step(self, action):
@@ -21,7 +21,7 @@ class CustomEnv(gym.Env):
         obs = self.pygame.observe()
         reward = self.pygame.evaluate()
         done = self.pygame.is_done()
-        return obs, reward, done, {}
+        return obs, reward, done, {}, {}
 
     def render(self, mode="human", close=False):
         self.pygame.view()
