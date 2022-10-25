801d39c1d747ceb4779b71ca6627e6bba2099de1
Lazarus <101668253+LazarusGroves@users.noreply.github.com>
2022-10-25 17:02:01 +0200

    Update setup.py AGAIN
    Co-Authored-By: 2303289 <102320150+2303289@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="Scrum-Dog-Millionaires",
+    name="ScrumDogMillionaires",
     version="0.0.1",
     install_requires=["gym==0.26.0", "pygame==2.1.2"],
 )
